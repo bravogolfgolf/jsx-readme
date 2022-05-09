4c14ba5dcb0bcfa414d39298f74bbd398b0c1f07
Brian Gibson <gibsonbg@me.com>
2022-05-08 22:21:10 -0400

    children={''} not needed

--- README.md.tsx
+++ README.md.tsx
@@ -3,7 +3,7 @@
 // functions from jsx-readme
 /* @jsx MD */
 /* @jsxFrag Fragment */
-import type { Component } from "jsx-md";
+import type {Component} from "jsx-md";
 import MD, {
   BadgesFromPkg,
   CodecovBadge,
@@ -23,34 +23,35 @@ import MD, {
   ContributorsSectionFromPkg,
   FundingSectionFromPkg,
 } from "./src";
-import { CodeBlock, Heading, InlineCode, LineBreak } from "jsx-md";
+import {BlockQuote, CodeBlock, Heading, InlineCode, LineBreak} from "jsx-md";
 import pkg from "./package.json";
 
 const Readme: Component = () => (
   <Fragment>
+    <BlockQuote>Test1{'/n'}Test2</BlockQuote>
     {/* Create a header with title, badges and description inferred from package.json */}
-    <TitleFromPkg pkg={pkg} />
-    <BadgesFromPkg pkg={pkg} />
+    <TitleFromPkg pkg={pkg}/>
+    <BadgesFromPkg pkg={pkg}/>
     {/* Add additional badges. */}
-    <LicenseBadge pkg={pkg} />
-    <CodecovBadge pkg={pkg} />
-    <GithubWorkflowBadge pkg={pkg} workflowName="Build and deploy" />
+    <LicenseBadge pkg={pkg}/>
+    <CodecovBadge pkg={pkg}/>
+    <GithubWorkflowBadge pkg={pkg} workflowName="Build and deploy"/>
     <DiscordBadge
       inviteLink="https://discord.com/invite/X9HRSK5"
       serverId="750063320614174871"
     />
-    <CLAAssistantBadge pkg={pkg} />
-    <HacktoberfestBadge pkg={pkg} suggestionLabel="good first issue" />
-    <LineBreak />
-    <DescriptionFromPkg pkg={pkg} />
+    <CLAAssistantBadge pkg={pkg}/>
+    <HacktoberfestBadge pkg={pkg} suggestionLabel="good first issue"/>
+    <LineBreak/>
+    <DescriptionFromPkg pkg={pkg}/>
     {/* You can use the components from jsx-md to build custom markdown. */}
     <Heading level={2}>🛠 Installation</Heading>
     Add <InlineCode>jsx-readme</InlineCode> and <InlineCode>ts-node</InlineCode>{" "}
     to your <InlineCode>devDependencies</InlineCode>.
-    <LineBreak />
+    <LineBreak/>
     <CodeBlock language="sh">npm i jsx-readme ts-node -D</CodeBlock>
     Add these configs to your <InlineCode>tsconfig.json</InlineCode>:
-    <LineBreak />
+    <LineBreak/>
     <CodeBlock language="json">
       {`
 {
@@ -64,7 +65,7 @@ const Readme: Component = () => (
     Create a README.MD template (you may copy the example from this repo
     examples/README.md.tsx and edit to your taste). Add the following script to
     your <InlineCode>package.json</InlineCode>:
-    <LineBreak />
+    <LineBreak/>
     <CodeBlock language="json">
       {`
 {
@@ -74,21 +75,21 @@ const Readme: Component = () => (
 }
       `}
     </CodeBlock>
-    <LineBreak />
-    <LineBreak />
+    <LineBreak/>
+    <LineBreak/>
     {/* Create an example section based on all files from the example directory set up in package.json */}
-    <ExamplesFromPkg pkg={pkg} />
+    <ExamplesFromPkg pkg={pkg}/>
     {/* Create a section linking to the homepage from package.json */}
-    <HomepageFromPkg pkg={pkg} />
+    <HomepageFromPkg pkg={pkg}/>
     {/* Create a section linking to the contributing guidelines file */}
-    <ContributingSection />
+    <ContributingSection/>
     {/* Create a section linking to the contributors of the repo */}
-    <ContributorsSectionFromPkg pkg={pkg} />
+    <ContributorsSectionFromPkg pkg={pkg}/>
     {/* Create a funding section based on funding information set up in package.json */}
-    <FundingSectionFromPkg pkg={pkg} />
+    <FundingSectionFromPkg pkg={pkg}/>
     {/* Create a section linking to the license file. */}
-    <LicenseFromPkg pkg={pkg} />
+    <LicenseFromPkg pkg={pkg}/>
   </Fragment>
 );
 
-void renderToFile("./README.md", <Readme />);
+void renderToFile("./README.md", <Readme/>);
